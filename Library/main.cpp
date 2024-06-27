790a5c1efd2b9705bc690525878369038f0a156d
dlskrud815 <dlskrud815@gmail.com>
2024-06-27 19:40:06 +0900

    Add project files.

---
+++ Library/main.cpp
@@ -0,0 +1,97 @@
+﻿#include <stdlib.h>
+#include <iostream>
+
+#include "mysql_connection.h"
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/prepared_statement.h>
+using namespace std;
+
+// for demonstration only. never save your password in the code!
+const string server = "tcp://127.0.0.1:3306"; // 데이터베이스 주소
+const string username = "user"; // 데이터베이스 사용자
+const string password = "1234"; // 데이터베이스 접속 비밀번호
+
+int main()
+{
+    // MySQL Connector/C++ 초기화
+    sql::Driver* driver; // 추후 해제하지 않아도 Connector/C++가 자동으로 해제해 줌
+    sql::Connection* con;
+    sql::Statement* stmt;
+    sql::ResultSet* result;
+    sql::PreparedStatement* pstmt;
+
+    try
+    {
+        driver = get_driver_instance();
+        con = driver->connect(server, username, password);
+    }
+    catch (sql::SQLException e)
+    {
+        cout << "Could not connect to server. Error message: " << e.what() << endl;
+        system("pause");
+        exit(1);
+    }
+
+    // please create database "cpp_db" ahead of time
+    con->setSchema("cpp_db");
+
+    // connector에서 한글 출력을 위한 셋팅 
+    stmt = con->createStatement(); // 추가!!
+    stmt->execute("set names euckr"); // 추가!!
+    if (stmt) { delete stmt; stmt = nullptr; } // 추가!!
+
+    // 데이터베이스 쿼리 실행
+    stmt = con->createStatement();
+    stmt->execute("DROP TABLE IF EXISTS inventory");
+    cout << "Finished dropping table (if existed)" << endl;
+
+    stmt->execute("CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);");
+    cout << "Finished creating table" << endl;
+    delete stmt;
+
+    pstmt = con->prepareStatement("INSERT INTO inventory(name, quantity) VALUES(?,?)");
+    pstmt->setString(1, "banana");
+    pstmt->setInt(2, 150);
+    pstmt->execute();
+    cout << "One row inserted." << endl;
+
+    pstmt->setString(1, "orange");
+    pstmt->setInt(2, 154);
+    pstmt->execute();
+    cout << "One row inserted." << endl;
+
+    pstmt->setString(1, "apple");
+    pstmt->setInt(2, 100);
+    pstmt->execute();
+    cout << "One row inserted." << endl;
+
+    //읽기
+    pstmt = con->prepareStatement("SELECT * FROM inventory;");
+    result = pstmt->executeQuery();
+
+    while (result->next())
+        printf("Reading from table=(%d, %s, %d)\n", result->getInt(1), result->getString(2).c_str(), result->getInt(3));
+
+
+    //업데이트
+    pstmt = con->prepareStatement("UPDATE inventory SET quantity = ? WHERE name = ?");
+    pstmt->setInt(1, 200); //quantity
+    pstmt->setString(2, "banana"); //name 바나나인 행에 quantity 150->200
+    pstmt->executeQuery();
+    printf("Row updated\n");
+
+    //삭제
+    pstmt = con->prepareStatement("DELETE FROM inventory WHERE name = ?");
+    pstmt->setString(1, "orange"); //orange 행 삭제
+    result = pstmt->executeQuery();
+    printf("Row deleted\n");
+
+    // MySQL Connector/C++ 정리
+    delete pstmt;
+    delete con;
+    delete result;
+    system("pause");
+    return 0;
+}
